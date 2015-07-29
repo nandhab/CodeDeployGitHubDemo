77a37c8f03002ddc2f042f88cfd88dfd366b4dd6
nandhalabs <nandhab@users.noreply.github.com>
2015-07-30 00:40:33 +0530

    Update mysql_table_create.sh

--- scripts/mysql_table_create.sh
+++ scripts/mysql_table_create.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-mysql -u localhost -u root -proot -D test < create.sql
+mysql -u localhost -u root -proot -D test << EOD1
+CREATE TABLE customers(
+  id CHAR (32) NOT NULL,
+  PRIMARY KEY(id),
+  first_name VARCHAR(64),
+  last_name VARCHAR(64),
+  email VARCHAR(64)
+);
+
+INSERT INTO customers ( id, first_name, last_name, email ) VALUES ( uuid(), 'Jane', 'Smith', 'jane.smith@example.com' );
+INSERT INTO customers ( id, first_name, last_name, email ) VALUES ( uuid(), 'Dave', 'Richards', 'dave.richards@example.com' );
+EOD1
