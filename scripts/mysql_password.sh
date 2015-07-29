88d342c0f922b9e69d26abc8c5678f542931e872
nandhalabs <nandhab@users.noreply.github.com>
2015-07-30 00:37:25 +0530

    Create mysql_password.sh

---
+++ scripts/mysql_password.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+mysql -u root << EOF
+UPDATE mysql.user SET Password=PASSWORD('root') WHERE User='root';
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+DELETE FROM mysql.user WHERE User='';
+DELETE FROM mysql.db WHERE Db='test' OR Db='test\_%';
+FLUSH PRIVILEGES;
+EOF
