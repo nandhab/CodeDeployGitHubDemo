659f38c1e62bc85340680e0e7ea412c8f029ae1a
nandhalabs <nandhab@users.noreply.github.com>
2015-08-01 21:25:33 +0530

    Update mysql_password.sh

--- scripts/mysql_password.sh
+++ scripts/mysql_password.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mysql -u root << EOF
+mysql -h practice.cd5vru9wzyku.us-west-2.rds.amazonaws.com -u root << EOF
 UPDATE mysql.user SET Password=PASSWORD('root') WHERE User='comverse';
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
 DELETE FROM mysql.user WHERE User='';
